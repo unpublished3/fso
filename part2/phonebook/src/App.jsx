003df69c82c34de701a113744bc24f58cb04903c
unpublished3 <chapagainsakshyam@gmail.com>
2024-09-04 16:50:27 +0545

    Complete 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,41 +1,53 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234556" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const onNameChange = (e) => {
     setNewName(e.target.value);
   };
 
-  const addNewName = (e) => {
+  const onNumberChange = (e) => {
+    setNewNumber(e.target.value);
+  };
+
+  const addNewPerson = (e) => {
     e.preventDefault();
-    if (persons.some((person) => newName  === person.name)) {
+    if (persons.some((person) => newName === person.name)) {
       alert(`${newName} is already added to the phonebook`);
       return;
     }
 
-    setPersons(persons.concat({ name: newName }));
+    setPersons(persons.concat({ name: newName, number: newNumber }));
     setNewName("");
+    setNewNumber("");
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addNewName}>
+      <form onSubmit={addNewPerson}>
         <div>
           Name: <input value={newName} onChange={onNameChange} />
         </div>
+        <div>
+          Number: <input value={newNumber} onChange={onNumberChange} />
+        </div>
+
         <div>
           <button type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map((person) => (
-          <li key={person.name}>{person.name}</li>
-        ))}
-      </ul>
+      {persons.map((person) => (
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
+      ))}
     </div>
   );
 };
