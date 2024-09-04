a4d2f70da6467cbb3ac15da7fa4ca4648abe94ba
unpublished3 <chapagainsakshyam@gmail.com>
2024-09-04 11:22:11 +0545

    Complete 2.2 and 2.3

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -3,16 +3,22 @@ const Course = ({ course }) => {
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
+      <Total
+        total={course.parts.reduce(
+          (total, current) => total + current.exercises,
+          0
+        )}
+      />
     </div>
   );
 };
 
 const Header = ({ course }) => <h1>{course}</h1>;
 
-const Content = ({ parts }) => (
-    parts.map(part => <Part part={part} key={part.id}/>)
-);
+const Content = ({ parts }) =>
+  parts.map((part) => <Part part={part} key={part.id} />);
 
+const Total = ({ total }) => <p>Number of exercises {total}</p>;
 
 const Part = ({ part }) => (
   <p>
